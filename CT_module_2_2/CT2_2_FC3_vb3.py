239fd1d665d5cefd7cf31fe749e956d77527b48d
h.massink <h.massink@avans.nl>
2025-11-27 13:46:59 +0100

    update

--- CT_module_2_2/CT2_2_FC3_vb3.py
+++ CT_module_2_2/CT2_2_FC3_vb3.py
@@ -42,35 +42,103 @@ x = min(x[x > 0])
 x = round(x, 1)
 
 wanddikte = x
+while True:
+    breedte_uitw = kies_getal_stap(8.0, 15.5, 0.5, decimalen)
 
-breedte_inw = breedte_uitw - 2 * wanddikte
+    hoogte_uitw = kies_getal_stap(5, 8.5, 0.5, decimalen)
+
+    randje = kies_getal_stap(0.2, 0.9, 0.1, decimalen)
+
+    h = hoogte_uitw - randje
+    O = breedte_uitw * h / 2.5
+    a = 4
+    b =(2 * breedte_uitw + 2 * hoogte_uitw)
+    c = O
+
+    x = np.roots([-a, b, -c])
+    x = min(x[x > 0])
+    x = round(x, 1)
+
+    wanddikte = x
+
+    breedte_inw = breedte_uitw - 2 * wanddikte
+
+    hoogte_inw = hoogte_uitw - 2 * wanddikte
+
+    lengte = kies_getal_stap(40, 90, 10, decimalen)
+
+    opp_m =breedte_uitw * hoogte_uitw - breedte_inw * hoogte_inw
+    gewicht_beton = opp_m * lengte * water.g * dichtheid_beton / 1000
+
+    druk_onderzijde = (gewicht_beton * 1000) / (lengte * breedte_uitw)
+
+    diepte = druk_onderzijde / (water.sgzoet * water.g)
+    rand = hoogte_uitw - diepte
+    if rand > 0.05:
+        break
+
+print(breedte_uitw) # [3]
+print(hoogte_uitw) # [4]
 print(round(breedte_inw, decimalen)) # [5]
-
-hoogte_inw = hoogte_uitw - 2 * wanddikte
 print(round(hoogte_inw, decimalen)) # [6]
-
-lengte = kies_getal_stap(40, 90, 10, decimalen)
 print(lengte) # [7]
-
-# Wat is het gewicht in KN
-
-opp_m =breedte_uitw * hoogte_uitw - breedte_inw * hoogte_inw
-gewicht_beton = opp_m * lengte * water.g * dichtheid_beton / 1000  # in kN
 print(round(gewicht_beton, 0)) # [8]
-
-# Hoe hoog steekt de tunnel boven het water uit?
-
-druk_onderzijde = (gewicht_beton * 1000) / (lengte * breedte_uitw)  # in Pa
 print(round(druk_onderzijde, decimalen)) # [9]
-
-diepte = druk_onderzijde / (water.sgzoet * water.g)  # in m
 print(round(diepte, decimalen)) # [10]
-
-rand = hoogte_uitw - diepte
 print(round(rand, decimalen)) # [11]
 
-volume_boven_water = rand * breedte_uitw * lengte  # in m3
-ballast = volume_boven_water * 1000  # l
+volume_boven_water = rand * breedte_uitw * lengte
+ballast = volume_boven_water * 1000
 print(round(ballast, 0)) # [12]
 
 #")
+
+def test_berekening():
+    successen = 0
+    mislukkingen = 0
+
+    for i in range(500):
+        max_pogingen = 500
+        for poging in range(max_pogingen):
+            breedte_uitw = kies_getal_stap(8.0, 15.5, 0.5, decimalen)
+            hoogte_uitw = kies_getal_stap(5, 8.5, 0.5, decimalen)
+            randje = kies_getal_stap(0.2, 0.9, 0.1, decimalen)
+
+            h = hoogte_uitw - randje
+            O = breedte_uitw * h / 2.5
+            a = 4
+            b = (2 * breedte_uitw + 2 * hoogte_uitw)
+            c = O
+
+            x = np.roots([-a, b, -c])
+            x = min(x[x > 0])
+            x = round(x, 1)
+            wanddikte = x
+
+            breedte_inw = breedte_uitw - 2 * wanddikte
+            hoogte_inw = hoogte_uitw - 2 * wanddikte
+            lengte = kies_getal_stap(40, 90, 10, decimalen)
+
+            opp_m = breedte_uitw * hoogte_uitw - breedte_inw * hoogte_inw
+            gewicht_beton = opp_m * lengte * water.g * dichtheid_beton / 1000
+            druk_onderzijde = (gewicht_beton * 1000) / (lengte * breedte_uitw)
+            diepte = druk_onderzijde / (water.sgzoet * water.g)
+            rand = hoogte_uitw - diepte
+
+            if rand > 0.05:
+                if rand > 0:
+                    successen += 1
+                else:
+                    mislukkingen += 1
+                    print(f"Test {i + 1}: rand = {rand:.3f} (niet positief!)")
+                break
+
+    print(f"\nResultaten:")
+    print(f"Successen (rand > 0): {successen}")
+    print(f"Mislukkingen (rand <= 0): {mislukkingen}")
+    print(f"Totaal: {successen + mislukkingen}")
+
+
+# Voer de test uit
+test_berekening()
+
